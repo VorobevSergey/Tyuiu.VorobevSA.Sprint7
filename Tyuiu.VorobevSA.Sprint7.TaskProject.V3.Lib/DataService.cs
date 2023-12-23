23817a9571e4193be394b06be6b4d9ed4b367baf
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-12-23 22:16:28 +0500

    Add project files.

---
+++ Tyuiu.VorobevSA.Sprint7.TaskProject.V3.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VorobevSA.Sprint7.TaskProject.V3.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.VorobevSA.Sprint7.TaskProject.V3.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.VorobevSA.Sprint7.TaskProject.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
